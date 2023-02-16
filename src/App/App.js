d0d3e2ecc077cc1400ec338d9f9512e03df51d2a
ReizalPambuko <rizalpambuko700@gmail.com>
2023-02-16 06:10:23 -0800

    Project pertama pake react

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import { Fragment } from 'react';
+import './App.css';
+import Navbar from '../Navbar';
+
+
+function App() {
+  return (
+    <Fragment>
+      <h1 className='text-red-500'>hh</h1>
+      <Navbar />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react";
+
+
+const Navbar = () => {
+    return(
+        <Fragment>
+        <div>
+            <div>
+                <div>
+                    <a href="" className='text-red-600'>REIZAL</a>
+                </div>
+            </div>
+        </div>
+        </Fragment>
+    )
+}
+
+export default Navbar;
